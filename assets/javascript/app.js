83b749caca70cb04fcf6483251b97e412095e7f6
Ian Applekamp <ieapplekamp@gmail.com>
2019-10-05 22:48:33 -0400

    saving the firebase key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,28 +18,46 @@ $(document).ready(function () {
 
     // might be better to store this information in a objust? idk
     // Globes
+    var totalPlayers = [];
     var wins = 0;   
     var losses = 0;
     var ties = 0;
+    var playerOne;
+    var playerTwo;
+
+    // Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyBJQRyWQIreKvpRXBtEu60k3JIYyCvUicU",
+        authDomain: "rps-game-86734.firebaseapp.com",
+        databaseURL: "https://rps-game-86734.firebaseio.com",
+        projectId: "rps-game-86734",
+        storageBucket: "rps-game-86734.appspot.com",
+        messagingSenderId: "204022287789",
+        appId: "1:204022287789:web:f20d87b8747f6b3d7ba524",
+        measurementId: "G-84WG0VZ41T"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+    //   firebase.analytics();
 
 
     // store the user variable 'r-p-s', match that to the opponent 
 
     $('#scissors').on('click', function () {
         
-        console.log($('.scissorsClick').attr("value"));
+        console.log($('.scissors').attr("value"));
 
     });
 
     $('#rock').on('click', function () {
         
-        console.log($('.rockClick').attr("value"));
+        console.log($('.rock').attr("value"));
 
     });
 
     $('#paper').on('click', function () {
         
-        console.log($('.paperClick').attr("value"));
+        console.log($('.paper').attr("value"));
 
     });
 
